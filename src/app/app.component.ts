61e31afdb04dd1813db357f00104ad7e74c01ed2
jonatas.oliveira <jonatas.oliveira@fornecedores.goias.gov.br>
2024-07-31 19:00:56 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,72 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {EditorModule} from 'primeng/editor';
+import {CardModule} from 'primeng/card';
+import {ListboxModule} from 'primeng/listbox';
+import {DialogModule} from 'primeng/dialog';
+import {Button} from 'primeng/button';
+import {AccordionModule} from 'primeng/accordion';
+import {NgFor} from '@angular/common';
+import {SplitterModule} from 'primeng/splitter';
+import {PanelModule} from 'primeng/panel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    NgFor,
+    CardModule,
+    AccordionModule,
+    ListboxModule,
+    FormsModule,
+    DialogModule,
+    EditorModule,
+    Button,
+    SplitterModule,
+    PanelModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'poc-editor-rico';
+  summaryElementos = [{
+    label: 'Elemento 1',
+    itens: [
+      {label: 'Item 1', content: '<p>Conteúdo do Item 1</p>'},
+      {label: 'Item 2', content: '<p>Conteúdo do Item 2</p>'},
+      {label: 'Item 3', content: '<p>Conteúdo do Item 3</p>'}
+    ]
+  },
+    {
+      label: 'Elemento 2',
+      itens: [
+        {label: 'Item 1', content: '<p>Conteúdo do Item 1</p>'},
+        {label: 'Item 2', content: '<p>Conteúdo do Item 2</p>'},
+        {label: 'Item 3', content: '<p>Conteúdo do Item 3</p>'}
+      ]
+    }
+  ]
+
+  text: string = '<div>Hello World!</div><div>PrimeNG <b>Editor</b> Rocks</div><div><br></div>';
+
+  selectedItem: any =  { label: '', content: '<p>Conteúdo do Item 1</p>' };
+  displayEditor = false;
+  documentContent = '';
+
+  openEditor(item: any) {
+    this.selectedItem = { ...item }; // Cria uma cópia do item selecionado
+    this.displayEditor = !this.displayEditor;
+  }
+
+  saveContent() {
+    this.displayEditor = false;
+    this.updateDocumentContent();
+  }
+
+  updateDocumentContent() {
+    this.documentContent += this.selectedItem.content;
+    this.selectedItem =  { label: '', content: '' };
+  }
+
 }
